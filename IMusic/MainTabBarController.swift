acc44aabf15e48ce1188789cc2819460622925d9
Sergey Lobanov <sergey.lobanov@orioninc.com>
2021-10-27 19:48:46 +0300

    Implement MainTabBarItem

---
+++ IMusic/MainTabBarController.swift
@@ -0,0 +1,42 @@
+//
+//  MainTabBarController.swift
+//  IMusic
+//
+//  Created by Sergey Lobanov on 27.10.2021.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tabBar.backgroundColor = .white
+        tabBar.tintColor = UIColor(red: 255, green: 0, blue: 96, alpha: 1)
+        
+        let searchViewController = SearchViewController()
+        let libraryViewController = ViewController()
+        
+        viewControllers = [
+            generateViewController(
+                rootViewController: searchViewController,
+                image: UIImage(named: "search")!,
+                title: "Search"),
+            generateViewController(
+                rootViewController: libraryViewController,
+                image: UIImage(named: "library")!,
+                title: "Library")
+        ]
+    }
+    
+    private func generateViewController(
+        rootViewController: UIViewController, image: UIImage, title: String) -> UIViewController {
+            let navigationViewController = UINavigationController(rootViewController: rootViewController)
+            navigationViewController.tabBarItem.image = image
+            navigationViewController.tabBarItem.title = title
+            rootViewController.navigationItem.title = title
+            navigationViewController.navigationBar.prefersLargeTitles = true
+            return navigationViewController
+        }
+}
---
+++ IMusic/SearchViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchViewController.swift
+//  IMusic
+//
+//  Created by Sergey Lobanov on 27.10.2021.
+//
+
+import UIKit
+
+class SearchViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+    }
+}
