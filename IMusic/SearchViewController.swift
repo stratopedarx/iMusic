b22b38b9a3560b4ad66c6ecfe3b5119e979b7b23
Sergey Lobanov <sergey.lobanov@orioninc.com>
2021-10-28 14:17:57 +0300

    Setup Search UITableViewController

--- IMusic/SearchViewController.swift
+++ IMusic/SearchViewController.swift
@@ -7,10 +7,60 @@
 
 import UIKit
 
+struct TrackModel {
+    var trackName: String
+    var artistName: String
+}
+
+
 class SearchViewController: UITableViewController {
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    let tracks = [TrackModel(trackName: "bad guy", artistName: "Billie Eilish"),
+                 TrackModel(trackName: "bury a friend", artistName: "Billie Eilish")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .blue
+        view.backgroundColor = .white
+        setupSearchBar()
+        
+        // ячейку надо зарегестрировать у конкретного tableView
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+    }
+    
+    private func setupSearchBar() {
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+
+        searchController.searchBar.delegate = self
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tracks.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        
+        let track = tracks[indexPath.row]
+        
+        var content = cell.defaultContentConfiguration()
+        content.text = "\(track.trackName)\n\(track.artistName)"
+        cell.textLabel?.numberOfLines = 2
+        content.image = UIImage(named: "Image")
+        cell.contentConfiguration = content
+
+        return cell
+    }
+}
+
+// что бы мы могли обработать то, что мы пишем в поисковом окне, надо нащ контроллер подписать под один делегат
+//  что бы компилятор понял кто будет выполнять этот метод,  `searchController.searchBar.delegate = self`
+extension SearchViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
     }
 }
