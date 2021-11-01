a0c05b78274761d0a8c143853ebf0f8134ae2236
Sergey Lobanov <sergey.lobanov@orioninc.com>
2021-11-01 17:40:44 +0300

    Add outlets and actions to trackDetailView

--- IMusic/Search/TrackDetailView/TrackDetailView.swift
+++ IMusic/Search/TrackDetailView/TrackDetailView.swift
@@ -9,7 +9,43 @@ import UIKit
 
 class TrackDetailView: UIView {
 
+    // Outlets
+    @IBOutlet private weak var trackImageView: UIImageView!
+    @IBOutlet private weak var currentTimeSlider: UISlider!
+    @IBOutlet private weak var currentTimeLabel: UILabel!
+    @IBOutlet private weak var durationLabel: UILabel!
+    @IBOutlet private weak var trackTitleLabel: UILabel!
+    @IBOutlet private weak var authorTitleLabel: UILabel!
+    @IBOutlet private weak var playPauseButton: UIButton!
+    @IBOutlet private weak var volumeSlider: UISlider!
+    
     override class func awakeFromNib() {
         super.awakeFromNib()
+        
+        
+        
     }
+    
+    // Actions
+    @IBAction private func dragDownButtonTapped(_ sender: UIButton) {
+        // сворачиваем данное окошко с экрана
+        self.removeFromSuperview()
+    }
+    
+    @IBAction private func handleCurrentTimeSlider(_ sender: UISlider) {
+    }
+    
+    
+    @IBAction private func handleVolumeSlider(_ sender: UISlider) {
+    }
+    
+    @IBAction private func previousTrack(_ sender: UIButton) {
+    }
+    
+    @IBAction private func playPauseAction(_ sender: UIButton) {
+    }
+
+    @IBAction private func nextTrack(_ sender: UIButton) {
+    }
+    
 }
